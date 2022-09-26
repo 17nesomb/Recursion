84c832077293e9904f695106ce96a359da71976f
Benjamin Nesom <17NesomB@fulford.york.sch.uk>
2022-09-26 14:55:15 +0100

    Added a calculator for the factorial of a number

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -10,6 +10,22 @@ namespace Recursion
     {
         static void Main(string[] args)
         {
+            // 5! means 5*4*3*2*1
+            Console.WriteLine("Welcome to Factorial Factory!");
+            Console.Write("Please enter a number: ");
+            int n = 0;
+            while(!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.Write("Please enter a number: ");
+            }
+            int factorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine($"{n}! is {factorial}");
         }
     }
 }
